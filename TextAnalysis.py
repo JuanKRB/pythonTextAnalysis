c99681185e7b5df5a266adaa15c8ff5e0ac703a5
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-07-19 15:20:15 -0600

    TextAnalysis

--- TextAnalysis.py
+++ TextAnalysis.py
@@ -1,9 +1,7 @@
 
-givenstrin g =("Lorem ipsum dolor! diam amet, consetetur Lorem magna. "
+givenstrin =("Lorem ipsum dolor! diam amet, consetetur Lorem magna. "
              "sed diam nonumy eirmod tempor. diam et labore? et diam "
              "magna. et diam amet.")
-
-
 
 class TextAnalyzer(object):
     def __init__(self, text):
@@ -15,8 +13,21 @@ class TextAnalyzer(object):
     def freqAll(self):
         wordList = self.text.split(' ')
         dict = {}
-        for word in set(wordList):  #
+        for word in set(wordList): # use set to remove duplicates in list
             dict[word] = wordList.count(word)
 
         return dict
+
+    def freqOf(self, word):
+        freqDict = self.freqAll()
+
+        if word in freqDict:
+            return freqDict[word]
+        else:
+            return 0
+
+Object = TextAnalyzer(givenstrin)
+print(Object.freqAll())
+
+print("It appears ", Object.freqOf("Lorem"))
 
